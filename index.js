f59de121b322793333910f8095d9603af4ece1b8
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2024-01-01 22:12:17 +0530

    more stack methods

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Stack {
   }
   // add element to top of stack
   push(element) {
-    console.log(this.count, "this.count from push start ");
+    // console.log(this.count, "this.count from push start ");
     this.items[this.count] = element;
     console.log(`${element} added to ${this.count}`);
     this.count += 1;
@@ -26,20 +26,49 @@ class Stack {
     }
     let deleteItem = this.items[this.count - 1];
     this.count -= 1;
+
+    //
     console.log(`${deleteItem} removed`);
     console.log(this.items, " this.items in pop");
     return deleteItem;
+  }
+
+  peek() {
+    console.log(`Top element is ${this.items[this.count - 1]}`);
+    return this.items[this.count - 1];
+  }
+  //
+  isEmpty() {
+    return this.count == 0 ? console.log("empty") : console.log("not empty");
+  }
+  //
+  size() {
+    return console.log(this.count, " size");
+  }
+  //
+  printElements() {
+    for (let index = 0; index < this.items.length; index++) {
+      const element = this.items[index];
+      console.log(element, " from print element");
+    }
   }
 }
 
 const stack = new Stack();
 
-// stack.push(100);
-// stack.push(200);
-// stack.push(300);
+stack.isEmpty();
+stack.push(100);
+//
+stack.isEmpty();
+stack.push(200);
+stack.push(300);
 
-// //
+//
 // stack.pop();
-const positionOfFirstElement = stack.push(100);
-//as push method is returning the position, last added item
-console.log(`First element added at position: ${positionOfFirstElement}`);
+//
+// stack.peek();
+// stack.isEmpty();
+//
+// stack.size();
+
+stack.printElements();
