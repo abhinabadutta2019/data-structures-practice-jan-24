cffe1c66c599fa2e669fc86244f36eccf1213167
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2024-01-05 21:04:33 +0530

    queue on object

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-// https://www.youtube.com/watch?v=NuBWJ7kIlDg&list=PLC3y8-rFHvwjPxNAKvZpdnsr41E0fCMMP&index=44
+// https://youtu.be/ba15sgOiAOg?si=scmGTbrVuHDKJn5q
 
 class Queue {
   constructor() {
-    this.items = [];
+    // this.items = [];
+    this.items = {};
+    this.rear = 0;
+    this.front = 0;
     // this.count = 0;
   }
   enqueue(element) {
-    this.items.push(element);
-    console.log(this.items, "from enqueue");
+    this.items[this.rear] = element;
+    this.rear += 1;
+    console.log(this.items);
+  }
+  dequeue() {
+    const item = this.items[this.front];
+
+    console.log(item, "item");
+    //
+    delete this.items[this.front];
+    this.front += 1;
+    console.log(this.items, "after delete");
+    //
+    console.log(this.rear, "rear");
+    console.log(this.front, "front");
   }
   //
-  dequeue(element) {
-    this.items.shift(element);
-    console.log(this.items, "from dequeue");
-  }
-  printItems() {
-    // console.log(this.items.toString());
+  watch() {
     console.log(this.items);
   }
 }
 
 const queue = new Queue();
 
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(40);
-// console.log(this.items);
-queue.printItems();
+queue.enqueue(100);
+queue.enqueue(200);
+queue.enqueue(300);
+queue.enqueue(400);
+//
 queue.dequeue();
-// queue.dequeue();
-// queue.printItems();
+queue.dequeue();
