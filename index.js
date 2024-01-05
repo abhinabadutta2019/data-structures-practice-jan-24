7fd7b8e3549b1dc9a03b113f5dfebee3ef2ecabd
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2024-01-05 19:26:13 +0530

    queue with array

--- index.js
+++ index.js
@@ -1,74 +1,32 @@
-class Stack {
+// https://www.youtube.com/watch?v=NuBWJ7kIlDg&list=PLC3y8-rFHvwjPxNAKvZpdnsr41E0fCMMP&index=44
+
+class Queue {
   constructor() {
     this.items = [];
-    this.count = 0;
+    // this.count = 0;
   }
-  // add element to top of stack
-  push(element) {
-    // console.log(this.count, "this.count from push start ");
-    this.items[this.count] = element;
-    console.log(`${element} added to ${this.count}`);
-    this.count += 1;
-    console.log(this.items, " this.items in push");
-
-    // this.count - 1;
-    // console.log(this.count, "this.count from push end ");
-    return this.count - 1;
-    // return this.count;
-
-    // return;
-  }
-  //remove top element in stack
-  // return undefined if stack is empty
-  pop() {
-    if (this.count == 0) {
-      return undefined;
-    }
-    let deleteItem = this.items[this.count - 1];
-    this.count -= 1;
-
-    //
-    console.log(`${deleteItem} removed`);
-    console.log(this.items, " this.items in pop");
-    return deleteItem;
-  }
-
-  peek() {
-    console.log(`Top element is ${this.items[this.count - 1]}`);
-    return this.items[this.count - 1];
+  enqueue(element) {
+    this.items.push(element);
+    console.log(this.items, "from enqueue");
   }
   //
-  isEmpty() {
-    return this.count == 0 ? console.log("empty") : console.log("not empty");
+  dequeue(element) {
+    this.items.shift(element);
+    console.log(this.items, "from dequeue");
   }
-  //
-  size() {
-    return console.log(this.count, " size");
-  }
-  //
-  printElements() {
-    for (let index = 0; index < this.items.length; index++) {
-      const element = this.items[index];
-      console.log(element, " from print element");
-    }
+  printItems() {
+    // console.log(this.items.toString());
+    console.log(this.items);
   }
 }
 
-const stack = new Stack();
+const queue = new Queue();
 
-stack.isEmpty();
-stack.push(100);
-//
-stack.isEmpty();
-stack.push(200);
-stack.push(300);
-
-//
-// stack.pop();
-//
-// stack.peek();
-// stack.isEmpty();
-//
-// stack.size();
-
-stack.printElements();
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(40);
+// console.log(this.items);
+queue.printItems();
+queue.dequeue();
+// queue.dequeue();
+// queue.printItems();
